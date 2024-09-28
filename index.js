90d2437503617aae1989f5859da1c924c5265c39
anonymous-acc-AA <aadityaa.atre@gmail.com>
2024-09-28 23:23:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "styles")));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+let tasks = [];
+
+app.get("/", (req, res) => {
+  res.render("index", { tasks });
+});
+
+app.post("/", (req, res) => {
+  const task = req.body.tasks;
+  if (!task || task.trim() === "") {
+  } else {
+    tasks.push(task);
+  }
+  res.redirect("/");
+});
+
+function clearArray() {
+  tasks = [];
+}
+
+app.post("/del", async (req, res) => {
+  await clearArray();
+  res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("Listening to the server");
+});
